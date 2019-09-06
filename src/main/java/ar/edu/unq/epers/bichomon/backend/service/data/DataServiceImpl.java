f3efe22e7b04b0908b1bdd6247940351f23cec85
FranSpinelli <franciscospinelli98@gmail.com>
2019-09-06 00:47:41 -0300

    DataServiceImpl created

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/service/data/DataServiceImpl.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.epers.bichomon.backend.service.data;
+
+public class DataServiceImpl implements DataService {
+
+    @Override
+    public void eliminarDatos() {
+
+    }
+
+    @Override
+    public void crearSetDatosIniciales() {
+
+    }
+}
