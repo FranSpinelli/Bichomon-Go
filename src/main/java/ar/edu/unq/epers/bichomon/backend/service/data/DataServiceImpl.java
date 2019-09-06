af4e7dff4c71e17c214fde79efd377369ce683ed
FranSpinelli <franciscospinelli98@gmail.com>
2019-09-06 16:59:29 -0300

    solved bugs in DataServiceimpl

--- src/main/java/ar/edu/unq/epers/bichomon/backend/service/data/DataServiceImpl.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/service/data/DataServiceImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import connection.ConnectionBlock;
 
 public class DataServiceImpl implements DataService {
 
