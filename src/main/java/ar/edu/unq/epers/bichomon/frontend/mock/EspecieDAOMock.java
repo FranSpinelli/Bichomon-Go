e2d450ad5127466dd72312230ef383e969ddf246
Francisco Svrznjak Spinelli <44419168+FranSpinelli@users.noreply.github.com>
2019-09-07 19:19:57 -0300

    modified Especie constructor calls in EspecieDaoMock (now Especie constructor hasnt receive an id)

--- src/main/java/ar/edu/unq/epers/bichomon/frontend/mock/EspecieDAOMock.java
+++ src/main/java/ar/edu/unq/epers/bichomon/frontend/mock/EspecieDAOMock.java
@@ -20,56 +20,56 @@ public class EspecieDAOMock implements EspecieDAO {
 	private static Map<String, Especie> DATA = new HashMap<>();
 	
 	static {
-		Especie red = new Especie(1, "Rojomon", TipoBicho.FUEGO);
+		Especie red = new Especie("Rojomon", TipoBicho.FUEGO);
 		red.setAltura(180);
 		red.setPeso(75);
 		red.setEnergiaIncial(100);
 		red.setUrlFoto("/image/rojomon.jpg");
 		DATA.put(red.getNombre(), red);
 		
-		Especie amarillo = new Especie(2, "Amarillomon", TipoBicho.ELECTRICIDAD);
+		Especie amarillo = new Especie( "Amarillomon", TipoBicho.ELECTRICIDAD);
 		amarillo.setAltura(170);
 		amarillo.setPeso(69);
 		amarillo.setEnergiaIncial(300);
 		amarillo.setUrlFoto("/image/amarillomon.png");
 		DATA.put(amarillo.getNombre(), amarillo);
 		
-		Especie green = new Especie(3, "Verdemon", TipoBicho.PLANTA);
+		Especie green = new Especie( "Verdemon", TipoBicho.PLANTA);
 		green.setAltura(150);
 		green.setPeso(55);
 		green.setEnergiaIncial(5000);
 		green.setUrlFoto("/image/verdemon.jpg");
 		DATA.put(green.getNombre(), green);
 		
-		Especie turronmon = new Especie(4, "Tierramon", TipoBicho.TIERRA);
+		Especie turronmon = new Especie( "Tierramon", TipoBicho.TIERRA);
 		turronmon.setAltura(1050);
 		turronmon.setPeso(99);
 		turronmon.setEnergiaIncial(5000);
 		turronmon.setUrlFoto("/image/tierramon.jpg");
 		DATA.put(turronmon.getNombre(), turronmon);
 		
-		Especie fantasmon = new Especie(5, "Fantasmon", TipoBicho.AIRE);
+		Especie fantasmon = new Especie( "Fantasmon", TipoBicho.AIRE);
 		fantasmon.setAltura(1050);
 		fantasmon.setPeso(99);
 		fantasmon.setEnergiaIncial(5000);
 		fantasmon.setUrlFoto("/image/fantasmon.jpg");
 		DATA.put(fantasmon.getNombre(), fantasmon);
 		
-		Especie vampiron = new Especie(6, "Vanpiron", TipoBicho.AIRE);
+		Especie vampiron = new Especie( "Vanpiron", TipoBicho.AIRE);
 		vampiron.setAltura(1050);
 		vampiron.setPeso(99);
 		vampiron.setEnergiaIncial(5000);
 		vampiron.setUrlFoto("/image/vampiromon.jpg");
 		DATA.put(vampiron.getNombre(), vampiron);
 		
-		Especie fortmon = new Especie(7, "Fortmon", TipoBicho.CHOCOLATE);
+		Especie fortmon = new Especie( "Fortmon", TipoBicho.CHOCOLATE);
 		fortmon.setAltura(1050);
 		fortmon.setPeso(99);
 		fortmon.setEnergiaIncial(5000);
 		fortmon.setUrlFoto("/image/fortmon.png");
 		DATA.put(fortmon.getNombre(), fortmon);
 		
-		Especie dientemon = new Especie(8, "Dientemon", TipoBicho.AGUA);
+		Especie dientemon = new Especie( "Dientemon", TipoBicho.AGUA);
 		dientemon.setAltura(1050);
 		dientemon.setPeso(99);
 		dientemon.setEnergiaIncial(5000);
