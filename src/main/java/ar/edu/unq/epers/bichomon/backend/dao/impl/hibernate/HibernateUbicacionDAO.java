0a3c6d3c1f8783c50142c153c8c9e73f24197ee5
adriancardozo <adrian.cardozo1@alu.unq.edu.ar>
2019-10-18 20:48:00 -0300

    Termino service mapa

--- src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/hibernate/HibernateUbicacionDAO.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/hibernate/HibernateUbicacionDAO.java
@@ -4,7 +4,6 @@ import ar.edu.unq.epers.bichomon.backend.dao.UbicacionDAO;
 import ar.edu.unq.epers.bichomon.backend.dao.impl.hibernate.exceptions.DojoNoUtilizado;
 import ar.edu.unq.epers.bichomon.backend.dao.impl.hibernate.exceptions.DojoSinCampeon;
 import ar.edu.unq.epers.bichomon.backend.model.bicho.Bicho;
-import ar.edu.unq.epers.bichomon.backend.model.bicho.Entrenador;
 import ar.edu.unq.epers.bichomon.backend.model.ubicacion.Dojo;
 import ar.edu.unq.epers.bichomon.backend.model.ubicacion.Ubicacion;
 import ar.edu.unq.epers.bichomon.backend.model.ubicacion.relacionadoADojo.Campeon;
@@ -14,9 +13,7 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 
 import javax.persistence.NoResultException;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class HibernateUbicacionDAO extends HibernateDAO<Ubicacion> implements UbicacionDAO {
     public HibernateUbicacionDAO() {
@@ -75,25 +72,24 @@ public class HibernateUbicacionDAO extends HibernateDAO<Ubicacion> implements Ub
 
     @Override
     public Bicho getCampeonHistorico(String dojo) {
-        Dojo dojoActual = (Dojo) this.recuperar(dojo);
+        Ubicacion dojoActual = this.recuperar(dojo);
         if(dojoActual == null){
             throw new UbicacionInexistente("La ubicacion no existe");
         }
-        if(dojoActual.getListaDeCampeones().isEmpty()){
-            throw new DojoNoUtilizado("El dojo no tuvo campeones");
-        }
         Session session = TransactionRunner.getCurrentSession();
-        //TODO: Preguntar como implementar la query con fechas
-        /*
 
-        String hql = "select bicho from Ubicacion ubicacion join ubicacion.campeonActual campeon join ubicacion.campeonesDelPasado campeonDelPasado join campeon.bicho bicho join campeonDelPasado.bicho bichoDelPasado where ubicacion.nombre = :nombre";//Ordenar por diferencia entre fechas
+       String hql = "select bicho from Campeon campeon join campeon.bicho bicho " +
+               "where campeon.id in (select campeon.id from Dojo ubicacion join ubicacion.campeonesDelPasado campeon where ubicacion.nombre = :nombre) " +
+               "or campeon.id in (select campeon.id from Dojo ubicacion join ubicacion.campeonActual campeon where ubicacion.nombre = :nombre) " +
+               "order by DATEDIFF(ifNull(campeon.fechaDeFin, NOW()), campeon.fechaDeInicio) desc";
 
         Query<Bicho> query = session.createQuery(hql);
         query.setParameter("nombre", dojo);
         query.setMaxResults(1);
-        return query.getSingleResult();
-*/
-        // TODO: Solucion provisoria no definitiva
-        return dojoActual.getListaDeCampeones().stream().max(Comparator.comparing(Campeon::cantidadDiasDeCampeonato)).get().getBicho();
+        try{
+            return query.getSingleResult();
+        }catch (NoResultException ex){
+            throw new DojoNoUtilizado("El dojo no tuvo campeones");
+        }
     }
 }
